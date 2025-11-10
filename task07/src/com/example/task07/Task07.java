51d58baf2ad44d31478cd6ae96e1cf45d7a1c912
riht7405 <fly200fly@gmail.com.com>
2025-11-11 01:44:40 +0500

    Низамов Айдар. Модуль 2. Задание 7

--- task07/src/com/example/task07/Task07.java
+++ task07/src/com/example/task07/Task07.java
@@ -3,19 +3,13 @@ package com.example.task07;
 public class Task07 {
 
     public static int solution(int n, int m, int k) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int Row = n / k;
+        int Column = m / k;
+        return Row * Column;
     }
 
     public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        int result = solution(10, 20, 5);
+        int result = solution(20, 50, 3);
         System.out.println(result);
-        */
     }
-
 }
